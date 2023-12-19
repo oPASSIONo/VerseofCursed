0b5205d868ef60a7d7ce5f44f03d71a3607178ec
oPASSIONo <pupak.atch@bumail.net>
2023-12-20 04:10:07 +0700

    not finish

--- Verse of Cursed/Assets/Script/PlayerController.cs
+++ Verse of Cursed/Assets/Script/PlayerController.cs
@@ -3,56 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 300f;
-    private bool isGrounded;
-
-    private Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        HandleInput();
-    }
-
-    void HandleInput()
+    [SerializeField] private float moveSpeed;
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+        var moveInput = (new Vector3(horizontal, 0, vertical)).normalized;
 
-        rb.MovePosition(transform.position + transform.TransformDirection(movement));
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-            Debug.Log("Jump!");
-
-            Jump();
-        }
-    }
-    
-
-    void Jump()
-    {
-        Debug.Log("Applying Jump Force!");
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        isGrounded = false;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
     }
 }
 
