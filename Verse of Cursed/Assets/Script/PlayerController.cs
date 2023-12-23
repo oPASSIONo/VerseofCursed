d4fcb4b087bee790fa05a2e84a47958c2c14378e
oPASSIONo <pupak.atch@bumail.net>
2023-12-23 11:20:24 +0700

    Add
    Jump/Mesh/Fix Player Control

--- Verse of Cursed/Assets/Script/PlayerController.cs
+++ Verse of Cursed/Assets/Script/PlayerController.cs
@@ -1,17 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.iOS;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 500f;
 
+    [Header("Ground Check Setting")]
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private Vector3 groundCheckOffset;
     [SerializeField] private LayerMask groundLayer;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private bool allowJump = true;
 
     private bool isGrounded;
 
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
 
     private CameraFollow cameraController;
     private CharacterController characterController;
+    private float ySpeed;
 
     private void Awake()
     {
@@ -35,21 +37,43 @@ public class PlayerController : MonoBehaviour
 
         var moveInput = (new Vector3(horizontal, 0, vertical)).normalized;
 
-        var moveDir =  cameraController.PlanarRotation * moveInput;
-        
-       GroundCheck();
-       Debug.Log("Isground = " + isGrounded);
+        var moveDir = cameraController.PlanarRotation * moveInput;
+
+        GroundCheck();
+
+        if (isGrounded)
+        {
+            ySpeed = -0.5f;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        var velocity = moveDir * moveSpeed;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
 
         if (moveAmount > 0)
         {
             characterController.Move(moveDir * moveSpeed * Time.deltaTime);
-            transform.position += moveInput * moveSpeed * Time.deltaTime;
             targetRotation = transform.rotation = Quaternion.LookRotation(moveDir);
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
     
+    void Jump()
+    {
+        float jumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpForce);
+        characterController.Move(Vector3.up * jumpVelocity * Time.deltaTime);
+    }
+
     void GroundCheck()
     {
         isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
@@ -61,7 +85,3 @@ public class PlayerController : MonoBehaviour
         Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
-
-
-
-
--- Verse of Cursed/Assets/Script/RaycastHit.cs
+++ Verse of Cursed/Assets/Script/RaycastHit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,57 @@ using UnityEngine;
 public class RaycastHit : MonoBehaviour
 {
     public float range = 5f;
-    void FixedUpdate()
+    [SerializeField] GameObject playerObject;
+    [SerializeField] GameObject sourceObject;
+
+    private void Awake()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RayShoot();
+
+        }
+    }
+
+    public void RayShoot()
     {
         Vector3 direction = Vector3.forward;
         Ray ray = new Ray(transform.position, transform.TransformDirection(direction * range));
-        Debug.DrawRay(transform.position,transform.TransformDirection(direction * range), Color.red);
+        Debug.DrawRay(transform.position, transform.TransformDirection(direction * range), Color.red);
         UnityEngine.RaycastHit hit;
- 
-        if (Physics.Raycast(ray, out hit , range))
+
+        if (Physics.Raycast(ray, out hit, range))
         {
-            Debug.Log("Ray hit: " + hit.collider.gameObject.name);
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                PullMesh();
+                Debug.Log("Ray hit: " + hit.collider.gameObject.name);
+            }
         }
     }
+    
+    public void PullMesh()
+    {
+        MeshFilter sourceMeshFilter = sourceObject.GetComponent<MeshFilter>();
+        MeshFilter playerMeshFilter = playerObject.GetComponent<MeshFilter>();
+
+        if (sourceMeshFilter != null && playerMeshFilter != null)
+        {
+            // Copy the mesh from sourceObject to playerObject
+            playerMeshFilter.mesh = sourceMeshFilter.mesh;
+        }
+        else
+        {
+            Debug.LogError("MeshFilter not found on sourceObject or playerObject.");
+        }
+    }
+    
+
 }
+
+
+
