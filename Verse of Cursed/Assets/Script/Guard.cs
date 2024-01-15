d594826182c01534efdf6e9c2dc15e91c0f7fd0b
oPASSIONo <pupak.atch@bumail.net>
2024-01-15 20:20:55 +0700

    Guard Turn To Poiny

--- Verse of Cursed/Assets/Script/Guard.cs
+++ Verse of Cursed/Assets/Script/Guard.cs
@@ -1,12 +1,12 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
     public float speed = 3;
     public float waitTime = 0.3f;
+    public float turnSpeed = 90f;
+    
     public Transform pathHolder;
 
     private void Start()
@@ -15,8 +15,9 @@ public class Guard : MonoBehaviour
         for (int i = 0; i < waypoints.Length; i++)
         {
             waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+            
         }
-
         StartCoroutine(FollowPath(waypoints));
     }
 
@@ -25,6 +26,7 @@ public class Guard : MonoBehaviour
         transform.position = waypoints[0];
         int targetWaypointIndex = 1;
         Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
 
         while (true)
         {
@@ -34,9 +36,23 @@ public class Guard : MonoBehaviour
                 targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                 targetWaypoint = waypoints[targetWaypointIndex];
                 yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
             }
 
             yield return null;
+        }
+    }
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg;
+
+        while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle )) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
+
         }
     }
     private void OnDrawGizmos()
@@ -45,8 +61,8 @@ public class Guard : MonoBehaviour
         Vector3 previousPosition = startPosition;
         foreach (Transform waypoint in pathHolder)
         {
-            Gizmos.DrawSphere(waypoint.position,.3f);
-            Gizmos.DrawLine(previousPosition,waypoint.position);
+            Gizmos.DrawSphere(waypoint.position,0.3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
             previousPosition = waypoint.position;
         }
         Gizmos.DrawLine(previousPosition, startPosition);
