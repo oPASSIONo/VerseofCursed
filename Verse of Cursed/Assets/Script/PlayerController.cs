342ba10df14b800e27cd014cb6099eaf46cb7e47
oPASSIONo <pupak.atch@bumail.net>
2024-01-15 15:01:45 +0700

    Revert "fall"
    This reverts commit 6eb34e73c0905a2e2ff8b73fd56c82d1f9e523bf.

--- Verse of Cursed/Assets/Script/PlayerController.cs
+++ Verse of Cursed/Assets/Script/PlayerController.cs
@@ -1,57 +1,57 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float rotationSpeed = 500f;
+
     [Header("Ground Check Setting")]
     [SerializeField] private float groundCheckRadius = 0.2f;
     [SerializeField] private Vector3 groundCheckOffset;
     [SerializeField] private LayerMask groundLayer;
+
     [Header("Jump Settings")]
     [SerializeField] private float jumpForce = 1000f;
-    [SerializeField] private bool jumping;
-    [Header("Falling Damage Settings")] 
-    [SerializeField] private float fallThresholdVelocity = 15f;
+    [SerializeField] private bool allowJump = true;
 
     private bool isGrounded;
-    private float ySpeed;
-    private Rigidbody rb;
 
     private Quaternion targetRotation;
 
     private CameraFollow cameraController;
     private CharacterController characterController;
+    private float ySpeed;
+    
+    [SerializeField] GameObject playerObject;
+    [SerializeField] GameObject player01;
+    [SerializeField] GameObject player02;
+    
 
-    [SerializeField] private GameObject playerObject;
-    [SerializeField] private GameObject player01;
-    [SerializeField] private GameObject player02;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraFollow>();
         characterController = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        bool previewGround = isGrounded;
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
         float moveAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+
         var moveInput = (new Vector3(horizontal, 0, vertical)).normalized;
+
         var moveDir = cameraController.PlanarRotation * moveInput;
 
         GroundCheck();
         SkillCheck();
-        
+
         if (isGrounded)
         {
             ySpeed = -0.5f;
-            if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
             }
@@ -61,13 +61,7 @@ public class PlayerController : MonoBehaviour
             ySpeed += Physics.gravity.y * Time.deltaTime;
         }
 
-        var velocity = moveDir * moveSpeed * Time.deltaTime;
-        if (!previewGround && isGrounded)
-        {
-            Debug.Log("Do damage : " + (characterController.velocity.y < -fallThresholdVelocity));
-            Debug.Log(characterController.velocity.y);
-        }
-        Debug.Log(characterController.velocity.y);
+        var velocity = moveDir * moveSpeed;
         velocity.y = ySpeed;
 
         characterController.Move(velocity * Time.deltaTime);
@@ -80,24 +74,24 @@ public class PlayerController : MonoBehaviour
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
-
+    
     void Jump()
     {
         float jumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpForce);
         characterController.Move(Vector3.up * jumpVelocity * Time.deltaTime);
     }
-    
+
     void GroundCheck()
     {
-        isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer, QueryTriggerInteraction.Ignore);
+        isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
     }
-    
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = new Color(0, 1, 0, 0.5f);
         Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
-
+    
     private void SkillCheck()
     {
         Mesh playerMesh = GetMesh(playerObject);
@@ -106,21 +100,23 @@ public class PlayerController : MonoBehaviour
 
         if (AreMeshesEqual(playerMesh, p1Mesh))
         {
-            moveSpeed = 9f;
+            Debug.Log("Player mesh is the same as p1 mesh.");
+            moveSpeed = 6f;
             jumpForce = 2000f;
         }
 
         if (AreMeshesEqual(playerMesh, p2Mesh))
         {
+            Debug.Log("Player mesh is the same as p2 mesh.");
             jumpForce = 30000f;
-            moveSpeed = 6f;
+            moveSpeed = 2f;
         }
     }
 
     Mesh GetMesh(GameObject obj)
     {
         MeshFilter meshFilter = obj.GetComponent<MeshFilter>();
-
+    
         if (meshFilter != null)
         {
             return meshFilter.sharedMesh;
@@ -134,6 +130,7 @@ public class PlayerController : MonoBehaviour
 
     bool AreMeshesEqual(Mesh mesh1, Mesh mesh2)
     {
+   
         return mesh1.vertices.Length == mesh2.vertices.Length;
     }
 }
